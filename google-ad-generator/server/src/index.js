4ef5ef4d8e8633d94550560bb5eb9e2f46cf907a
Sangeeth Francis James <sangeeth@adreport.io>
2024-09-13 00:14:16 -0400

    Fix deployment configuration

--- google-ad-generator/server/src/index.js
+++ google-ad-generator/server/src/index.js
@@ -1,6 +1,7 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5001;
@@ -10,12 +11,15 @@ app.use(cors());
 
 app.use(express.json());
 
-const adGeneratorRouter = require('./routes/adGenerator');
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../../client/build')));
 
+const adGeneratorRouter = require('./routes/adGenerator');
 app.use('/api/ad-generator', adGeneratorRouter);
 
-app.get('/', (req, res) => {
-  res.send('Google Ad Generator API');
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
 });
 
 app.listen(port, () => {
